8065ee4449a6be563226d34448bce158b0fae711
Sammad Ijaz <sammadijaz11223@gmail.com>
2026-02-28 02:36:47 +0500

    Implement basic number guessing prompt and input handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Guess the Number!");
+
+    print!("Please input your guess: ");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read the line");
+    
+    print!("You guessed: {guess}");
 }
