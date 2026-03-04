ade138a2f27b572a645ca9ab2eba848e428b458f
Sammad Ijaz <sammadijaz11223@gmail.com>
2026-03-05 03:50:00 +0500

    Handling invalid input without crashing the game

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read the line");
 
-        let guess: u32 = guess.trim().parse().expect("Please only enter a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
